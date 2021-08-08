d105bc27b6303dbd68d06615a5f7e07a6a310c8c
DongSubKim <dskim9218@gmail.com>
2021-08-09 02:52:56 +0900

    Add Chain of Responsibility

---
+++ src/main/java/blog/dskim/designPatterns/chainOfResponsibility/middleware/Middleware.java
@@ -0,0 +1,24 @@
+package blog.dskim.designPatterns.chainOfResponsibility.middleware;
+
+// Base middleware class.
+public abstract class Middleware {
+    private Middleware next;
+
+    // Builds chains of middleware objects.
+    public Middleware linkWith(Middleware next) {
+        this.next = next;
+        return next;
+    }
+
+    // subclasses will implement this method with concrete checks.
+    public abstract boolean check(String email, String password);
+
+    // Runs check on the next object in chain or ends traversing if we're in
+    // last object in chain
+    protected boolean checkNext(String email, String password) {
+        if (next == null) {
+            return true;
+        }
+        return next.check(email, password);
+    }
+}
---
+++ src/main/java/blog/dskim/designPatterns/chainOfResponsibility/middleware/RoleCheckMiddleware.java
@@ -0,0 +1,14 @@
+package blog.dskim.designPatterns.chainOfResponsibility.middleware;
+
+// ConcreteHandler. Checks a user's role.
+public class RoleCheckMiddleware extends Middleware{
+    @Override
+    public boolean check(String email, String password) {
+        if (email.equals("admin@example.com")) {
+            System.out.println("Hello, admin!");
+            return true;
+        }
+        System.out.println("Hello, user!");
+        return checkNext(email, password);
+    }
+}
